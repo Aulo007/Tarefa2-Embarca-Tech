e578b77f22f63aa24499bdc67b1aebbf588df7cb
Aulo Cezar <112286959+Aulo007@users.noreply.github.com>
2025-01-19 19:29:37 -0300

    Cria função para inicializar os leds e o buzzer

--- Tarefa2-Embarca-Tech.c
+++ Tarefa2-Embarca-Tech.c
@@ -3,8 +3,22 @@
 
 // Definição dos pinos
 
-#define LED_GREEN   11
-#define LED_BLUE    12
-#define LED_RED     13
-#define BUZZER      21
+#define LED_GREEN 11
+#define LED_BLUE 12
+#define LED_RED 13
+#define BUZZER 21
 
+void init_gpio()
+{
+    // Inicializa LEDs
+    gpio_init(LED_GREEN);
+    gpio_init(LED_BLUE);
+    gpio_init(LED_RED);
+    gpio_set_dir(LED_GREEN, GPIO_OUT);
+    gpio_set_dir(LED_BLUE, GPIO_OUT);
+    gpio_set_dir(LED_RED, GPIO_OUT);
+
+    // Inicializa Buzzer
+    gpio_init(BUZZER);
+    gpio_set_dir(BUZZER, GPIO_OUT);
+}
